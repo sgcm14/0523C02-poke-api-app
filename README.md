b21045bdedb3f08dfe8677fb360d5d2735350cc8
sgcm14 <sgcm14@gmail.com>
2024-10-05 22:43:31 -0500

    cleaning code

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+Aplicando useEffect y Axios
+=============
+- Este proyecto lo estoy realizando dentro del curso **Front End III** de la carrera [Certified Tech Developer](https://www.digitalhouse.com/ar/productos/programacion/certified-tech-developer "Certified Tech Developer") 
+- **Periodo :** Julio - Septiembre 2024
+> En este proyecto se practica react
 
-Hacer un llamado de la api utilizando PokeApi (u otra que se accesible) y renderizar los datos (preferiblemente nombre e Imagen de frente ) que pueda extrer.
-Para esto necesitaremos UserEffect y Fetch o Axios
+Hacer un llamado de la api utilizando **PokeApi** (u otra que se accesible) y renderizar los datos (preferiblemente nombre e Imagen de frente ) que se pueda extraer.
+
+Para esto necesitaremos **UseEffect** y **Fetch o Axios**
+
+**Realizado por :** Sammy Gigi Cantoral Montejo (sgcm14)
+
+<img src ="https://raw.githubusercontent.com/sgcm14/sgcm14/main/sammy.jpg" width="200">
--- src/components/PokemonAxios.jsx
+++ src/components/PokemonAxios.jsx
@@ -8,13 +8,6 @@ const PokemonAxios = () => {
   const [pokemon, setPokemon] = useState();
 
   useEffect(() => {
-    // const axiosData = async () => {
-    //   let res = await axios(url);
-    //   setPokemon(res.data.results);
-    //   setTimeout(() => {
-    //     setLoading(false);
-    //   }, 2000);
-    // };
     const axiosData = async () => {
       let res = await axios(url);
       const pokemonDetails = await Promise.all(
@@ -36,20 +29,6 @@ const PokemonAxios = () => {
       {loading ? (
         "Cargando pokemón..."
       ) : (
-        // <>
-        // <h1>{pokemon?.name}</h1>
-        // <img src={pokemon?.sprites.front_shiny_female} alt="" />
-        // <p>{pokemon?.types[0].type.name}</p>
-        // </>
-
-        // <div className="pokemon-list">
-        //   {pokemon.map((pokemon, index) => (
-        //     <div key={index} className="pokemon-item">
-        //       <h1>{pokemon.name}</h1>
-        //       {/* Aquí podrías agregar más detalles si haces otra solicitud para obtener la información completa de cada Pokémon */}
-        //     </div>
-        //   ))}
-        // </div>
         <div className="pokemon-list">
         {pokemon.map((pokemon, index) => (
           <div key={index} className="pokemon-item">
